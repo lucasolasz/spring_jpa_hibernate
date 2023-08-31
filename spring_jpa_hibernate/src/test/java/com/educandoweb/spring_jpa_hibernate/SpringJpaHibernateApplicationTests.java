18ab53cd4c0a9ca9dac8e210399bfca04ccc630c
conuaz <lucas.olasz@tty2000.com.br>
2023-08-31 14:21:03 -0300

    Inserindo projeto base

---
+++ spring_jpa_hibernate/src/test/java/com/educandoweb/spring_jpa_hibernate/SpringJpaHibernateApplicationTests.java
@@ -0,0 +1,13 @@
+package com.educandoweb.spring_jpa_hibernate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringJpaHibernateApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
