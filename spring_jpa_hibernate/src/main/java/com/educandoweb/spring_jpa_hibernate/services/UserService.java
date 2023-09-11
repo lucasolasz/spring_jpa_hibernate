164bb19cb9c56ba0d6cd327e14a5f042563d18ed
conuaz <lucas.olasz@tty2000.com.br>
2023-09-11 15:17:32 -0300

    Service layer, component registration

---
+++ spring_jpa_hibernate/src/main/java/com/educandoweb/spring_jpa_hibernate/services/UserService.java
@@ -0,0 +1,26 @@
+package com.educandoweb.spring_jpa_hibernate.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.educandoweb.spring_jpa_hibernate.entities.User;
+import com.educandoweb.spring_jpa_hibernate.repositories.UserRepository;
+
+@Service
+public class UserService {
+
+	@Autowired
+	private UserRepository repository;
+
+	public List<User> findAll() {
+		return repository.findAll();
+	}
+	
+	public User findById(Long id) {
+		Optional<User> obj = repository.findById(id);
+		return obj.get();
+	}
+}
