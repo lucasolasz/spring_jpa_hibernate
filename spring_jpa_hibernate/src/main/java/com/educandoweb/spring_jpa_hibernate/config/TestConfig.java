f9a2dd1d545dccb97e9b2b48911276aa27441f73
conuaz <lucas.olasz@tty2000.com.br>
2023-09-26 16:28:31 -0300

    Payment, one-to-one association

--- spring_jpa_hibernate/src/main/java/com/educandoweb/spring_jpa_hibernate/config/TestConfig.java
+++ spring_jpa_hibernate/src/main/java/com/educandoweb/spring_jpa_hibernate/config/TestConfig.java
@@ -11,6 +11,7 @@ import org.springframework.context.annotation.Profile;
 import com.educandoweb.spring_jpa_hibernate.entities.Category;
 import com.educandoweb.spring_jpa_hibernate.entities.Order;
 import com.educandoweb.spring_jpa_hibernate.entities.OrderItem;
+import com.educandoweb.spring_jpa_hibernate.entities.Payment;
 import com.educandoweb.spring_jpa_hibernate.entities.Product;
 import com.educandoweb.spring_jpa_hibernate.entities.User;
 import com.educandoweb.spring_jpa_hibernate.entities.enums.OrderStatus;
@@ -35,7 +36,7 @@ public class TestConfig implements CommandLineRunner {
 
 	@Autowired
 	private ProductRepository productRepository;
-	
+
 	@Autowired
 	private OrderItemRepository orderItemRepository;
 
@@ -74,13 +75,18 @@ public class TestConfig implements CommandLineRunner {
 
 		userRepository.saveAll(Arrays.asList(u1, u2));
 		orderRepository.saveAll(Arrays.asList(o1, o2, o3));
-		
-		OrderItem oi1 = new OrderItem(o1, p1, 2, p1.getPrice()); 
-		OrderItem oi2 = new OrderItem(o1, p3, 1, p3.getPrice()); 
-		OrderItem oi3 = new OrderItem(o2, p3, 2, p3.getPrice()); 
-		OrderItem oi4 = new OrderItem(o3, p5, 2, p5.getPrice()); 
-		
+
+		OrderItem oi1 = new OrderItem(o1, p1, 2, p1.getPrice());
+		OrderItem oi2 = new OrderItem(o1, p3, 1, p3.getPrice());
+		OrderItem oi3 = new OrderItem(o2, p3, 2, p3.getPrice());
+		OrderItem oi4 = new OrderItem(o3, p5, 2, p5.getPrice());
+
 		orderItemRepository.saveAll(Arrays.asList(oi1, oi2, oi3, oi4));
+
+		Payment pay1 = new Payment(null, Instant.parse("2019-06-20T21:53:07Z"), o1);
+		o1.setPayment(pay1);
+
+		orderRepository.save(o1);
 
 	}
 
